8ed2866c34cc7fe7fc7097b76e25de856b6a17ba
Marcin Tatoń <tatonmarcinpr@gmail.com>
2022-09-24 00:57:03 +0200

    Testing deployment

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ dotenv.config();
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/other', (req, res) => {
+  res.send('Hello World! other');
+});
+
 app.listen(3001, () => {
   console.log(`Server running on 3001`);
 });
