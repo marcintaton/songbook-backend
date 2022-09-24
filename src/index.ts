f44f37a2ccf8d4e5a684ce2f5912ada23cdde2e3
Marcin Tatoń <tatonmarcinpr@gmail.com>
2022-09-24 02:39:29 +0200

    Mongo connection added and tested

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
 
+mongoose.connect(
+  `mongodb+srv://admin:${process.env.DB_PASSWORD}@songbook.s3sbnxb.mongodb.net/?retryWrites=true&w=majority`,
+  { dbName: 'songbook' }
+);
+
+const songSchema = new mongoose.Schema({
+  title: String,
+});
+
+const Song = mongoose.model('songs', songSchema);
+
+// Get the default connection
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/other', (req, res) => {
-  res.send('Hello World! other');
+app.get('/other', async (req, res) => {
+  const response = await Song.find({});
+  res.send(response);
 });
 
 app.listen(process.env.PORT || 3001, () => {
-  console.log(`Server running on 3001`);
+  console.log(`Server running`);
 });
