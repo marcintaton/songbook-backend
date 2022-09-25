91ea3b5e5071da0b39cfef32644fa17b3e2f44de
Marcin Tatoń <tatonmarcinpr@gmail.com>
2022-09-25 22:26:35 +0200

    tag substitution and sorting

--- src/routes/songs.ts
+++ src/routes/songs.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 import Song from '@src/models/songs';
+import Tag from '@src/models/tags';
 
 const router = Router();
 
@@ -8,9 +9,20 @@ router.get('/', async (req: Request, res: Response) => {
   res.status(200).send(songs);
 });
 
-router.get('/metadata', async (req: Request, res: Response) => {
-  const songs = await Song.find().select('title tags');
-  res.status(200).send(songs);
+router.get('/metadata/all', async (req: Request, res: Response) => {
+  const songs = await Song.find().select('title tags').sort('title');
+  const tags = await Tag.find();
+  const songsWithTags = songs.map((song) => {
+    return {
+      ...song.toObject(),
+      tags: song.tags.map(
+        // eslint-disable-next-line no-underscore-dangle
+        (tag) => tags.find((x) => x._id.toHexString() === tag)?.name
+      ),
+    };
+  });
+
+  res.status(200).send(songsWithTags);
 });
 
 router.get('/:id', async (req: Request, res: Response) => {
